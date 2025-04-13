9ea761e9b4b29316968c52de90b36d14debee488
snehasish-dev <snehasishdeb439@gmail.com>
2025-04-13 08:08:31 +0530

    Add List Methods

---
+++ list_methods.py
@@ -0,0 +1,9 @@
+list = [123,3,4,5,6,7,7]
+#list append method
+list.append(500)
+print("New list is:",list)
+#list count method
+print(list.count(7))
+#List replace method
+list.sort(reverse=True)
+print(list)
