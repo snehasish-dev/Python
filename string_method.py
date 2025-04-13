02d77cc2ce73a102c5a855af72fa9126e1b3d4ea
snehasish-dev <snehasishdeb439@gmail.com>
2025-04-13 08:15:19 +0530

    String Method list

---
+++ string_method.py
@@ -0,0 +1,13 @@
+name = "Snehasish Deb"
+# String replace
+name.replace("Snehasish","Sangita")
+#String count
+print(name.count('s'))
+#String len
+print(len(name))
+#String upper
+print(name.upper())
+#String lower
+print(name.lower())
+#String split
+print(name.split())
