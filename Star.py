33e9b5b3dcdf0a08e63ac22f473cb5dea28c3eaa
snehasish-dev <snehasishdeb439@gmail.com>
2025-05-13 16:36:31 +0530

    Add the string line

--- Star.py
+++ Star.py
@@ -1,6 +1,9 @@
 n = int(input("How many row you are print:"))
-for i in range(1,n):
+for i in range(1,n+1):
     print(" "*(n-i),end="")
     print("*"*(2*(i-1)+1))
-
+print("\n")
+name = "snehasish"
+for i in range(len(name)):
+    print(name[i])
     
